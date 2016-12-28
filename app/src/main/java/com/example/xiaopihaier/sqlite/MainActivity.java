5436753d488518817a76b7c3335481616404e415
xiaopihaier <1428518169@qq.com>
2016-12-28 11:12:35 +0800

    SQLite

--- app/src/main/java/com/example/xiaopihaier/sqlite/MainActivity.java
+++ app/src/main/java/com/example/xiaopihaier/sqlite/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     //定义增删改查四个按钮
     Button add,delete,modify,query;
+
+    //定义四个输入框按钮
+    EditText add_edi,delete_edi,modify_edi,query_edi;
 
     //定义结果显示控件
     TextView Result;
@@ -23,17 +27,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void IntentView() {
 
+        //增加输入框
+        add_edi= (EditText) findViewById(R.id.add_edi);
+
         //增加按钮监听事件
         add= (Button) findViewById(R.id.add);
         add.setOnClickListener(this);
+
+        //删除输入框
+        delete_edi= (EditText) findViewById(R.id.add_edi);
 
         //删除按钮监听事件
         delete= (Button) findViewById(R.id.delete);
         delete.setOnClickListener(this);
 
+        //修改输入框
+        modify_edi= (EditText) findViewById(R.id.modify_edi);
+
         //修改按钮监听事件
         modify= (Button) findViewById(R.id.modify);
         modify.setOnClickListener(this);
+
+        //查询输入框
+        query_edi= (EditText) findViewById(R.id.query_edi);
 
         //查询按钮监听事件
         query= (Button) findViewById(R.id.query);
